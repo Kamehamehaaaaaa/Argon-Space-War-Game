51c9bd357674ee82be844b2aa4a06c8e14dd3e7c
Kamehamehaaaaaa <sushantsamani@gmail.com>
2023-03-21 14:55:23 +0530

    Includes basic for Argon Space War game - Terrain Building - Spaceship - Timeline Generation - Bug Fix - Enemy movement - Laser shooting - Collision and Triggers

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        // Debug.Log("Player ship bumped into enemy");
+        Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        // Debug.Log("Player ship bumped into trigger");
+        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+    }
+}
